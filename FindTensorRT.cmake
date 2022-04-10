6fc13a3afd55abb71643fb10d6ded034186a0169
Nicolas Iragne <nicoragne@hotmail.fr>
2022-04-10 18:30:30 +0200

    Fix FindTensorRT.cmake reporting NOTFOUND in case of nested find_package calls

--- FindTensorRT.cmake
+++ FindTensorRT.cmake
@@ -16,24 +16,26 @@ function(create_target TARGET)
   set(TARGET_FULL ${${TARGET}_LIBRARY})
   message(STATUS "Found ${TARGET} at ${TARGET_FULL}")
   string(FIND ${TARGET_FULL} "NOTFOUND" _VAR)
-  if(${_VAR} EQUAL "-1" AND NOT TARGET trt::${TARGET})
-    set(TensorRT_${TARGET}_FOUND TRUE CACHE STRING "1")
-    get_filename_component(TARGET_FILE_NAME ${TARGET_FULL} NAME_WE)
-    get_filename_component(TARGET_PATH ${TARGET_FULL} DIRECTORY)
-    add_library(trt::${TARGET} SHARED IMPORTED)
-    if(WIN32)
-      set_target_properties(
-        trt::${TARGET}
-        PROPERTIES IMPORTED_LOCATION "${TARGET_PATH}/${TARGET_FILE_NAME}.dll"
-                   IMPORTED_IMPLIB "${TARGET_PATH}/${TARGET_FILE_NAME}.lib"
-                   INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_DIR}/include")
-    else()
-      set_target_properties(trt::${TARGET} PROPERTIES IMPORTED_LOCATION "${TARGET_FULL}" INTERFACE_INCLUDE_DIRECTORIES
-                                                                                         "${TensorRT_DIR}/include")
-    endif()
-    install(IMPORTED_RUNTIME_ARTIFACTS trt::${TARGET} RUNTIME_DEPENDENCY_SET trt_set COMPONENT ${TARGET})
-    message("Created target: trt::${TARGET}")
-    set(TensorRT_${TARGET}_FOUND "TensorRT_${TARGET}_FOUND" CACHE INTERNAL "Found ${TARGET}")
+  if(${_VAR} EQUAL "-1")
+    if (NOT TARGET trt::${TARGET})
+      set(TensorRT_${TARGET}_FOUND TRUE CACHE STRING "1")
+      get_filename_component(TARGET_FILE_NAME ${TARGET_FULL} NAME_WE)
+      get_filename_component(TARGET_PATH ${TARGET_FULL} DIRECTORY)
+      add_library(trt::${TARGET} SHARED IMPORTED)
+      if(WIN32)
+        set_target_properties(
+          trt::${TARGET}
+          PROPERTIES IMPORTED_LOCATION "${TARGET_PATH}/${TARGET_FILE_NAME}.dll"
+                    IMPORTED_IMPLIB "${TARGET_PATH}/${TARGET_FILE_NAME}.lib"
+                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_DIR}/include")
+      else()
+        set_target_properties(trt::${TARGET} PROPERTIES IMPORTED_LOCATION "${TARGET_FULL}" INTERFACE_INCLUDE_DIRECTORIES
+                                                                                          "${TensorRT_DIR}/include")
+      endif(WIN32)
+      install(IMPORTED_RUNTIME_ARTIFACTS trt::${TARGET} RUNTIME_DEPENDENCY_SET trt_set COMPONENT ${TARGET})
+      message("Created target: trt::${TARGET}")
+      set(TensorRT_${TARGET}_FOUND "TensorRT_${TARGET}_FOUND" CACHE INTERNAL "Found ${TARGET}")
+    endif(NOT TARGET trt::${TARGET})
   else()
     set(TensorRT_${TARGET}_FOUND FALSE CACHE STRING "1")
     message("Library ${TARGET} not found!")
