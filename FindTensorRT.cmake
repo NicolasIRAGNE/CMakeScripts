fec3f88deca43eaac6a4bca124a3a807480fa8bc
Nicolas IRAGNE <nicolas.iragne@alyce.fr>
2022-07-14 23:21:27 +0200

    Partial fix for FindTensorRT.cmake not resolving symlinks. This will still cause issues when there are multiple chained symlinks. See https://gitlab.kitware.com/cmake/cmake/-/issues/23249

--- FindTensorRT.cmake
+++ FindTensorRT.cmake
@@ -21,16 +21,22 @@ function(create_target TARGET)
       set(TensorRT_${TARGET}_FOUND TRUE CACHE STRING "1")
       get_filename_component(TARGET_FILE_NAME ${TARGET_FULL} NAME_WE)
       get_filename_component(TARGET_PATH ${TARGET_FULL} DIRECTORY)
+      get_filename_component(TARGET_REALPATH ${TARGET_FULL} REALPATH)
+      get_filename_component(TARGET_SONAME ${TARGET_FULL} NAME)
       add_library(trt::${TARGET} SHARED IMPORTED)
       if(WIN32)
-        set_target_properties(
-          trt::${TARGET}
-          PROPERTIES IMPORTED_LOCATION "${TARGET_PATH}/${TARGET_FILE_NAME}.dll"
-                    IMPORTED_IMPLIB "${TARGET_PATH}/${TARGET_FILE_NAME}.lib"
-                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_DIR}/include")
+        set_target_properties(trt::${TARGET}
+          PROPERTIES
+            IMPORTED_LOCATION "${TARGET_PATH}/${TARGET_FILE_NAME}.dll"
+            IMPORTED_IMPLIB "${TARGET_PATH}/${TARGET_FILE_NAME}.lib"
+            INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_DIR}/include")
       else()
-        set_target_properties(trt::${TARGET} PROPERTIES IMPORTED_LOCATION "${TARGET_FULL}" INTERFACE_INCLUDE_DIRECTORIES
-                                                                                          "${TensorRT_DIR}/include")
+        set_target_properties(trt::${TARGET}
+          PROPERTIES
+            IMPORTED_LOCATION ${TARGET_REALPATH}
+            IMPORTED_SONAME ${TARGET_SONAME}
+            INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_DIR}/include"
+          )
       endif(WIN32)
       install(IMPORTED_RUNTIME_ARTIFACTS trt::${TARGET} RUNTIME_DEPENDENCY_SET trt_set COMPONENT ${TARGET})
       message("Created target: trt::${TARGET}")
@@ -51,16 +57,16 @@ endfunction()
 message("Finding ${TensorRT_FIND_COMPONENTS}")
 create_targets(${TensorRT_FIND_COMPONENTS})
 
-install(
-  RUNTIME_DEPENDENCY_SET
-  trt_set
-  PRE_EXCLUDE_REGEXES
-  "api-ms-*"
-  "ext-ms-*"
-  POST_EXCLUDE_REGEXES
-  ".*system32/.*\\.dll"
-  DIRECTORIES
-  ${CUDA_TOOLKIT_ROOT_DIR}/bin)
+# install(
+#   RUNTIME_DEPENDENCY_SET
+#   trt_set
+#   PRE_EXCLUDE_REGEXES
+#   "api-ms-*"
+#   "ext-ms-*"
+#   POST_EXCLUDE_REGEXES
+#   ".*system32/.*\\.dll"
+#   DIRECTORIES
+#   ${CUDA_TOOLKIT_ROOT_DIR}/bin)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TensorRT HANDLE_COMPONENTS)
